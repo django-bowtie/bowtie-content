7cb6fad5f93204669d3351ffc86851c688fd2e42
Rense VanderHoek <rense@me.com>
2016-05-25 15:02:22 +0200

    sir-trevor init js

--- bowtie_content/static/bowtie-content/init.js
+++ bowtie_content/static/bowtie-content/init.js
@@ -1,13 +1,22 @@
-(function($) {
-    $('.js-st-instance').each(function(i,el) {
-        var $el = $(el),
-            defaults = $el.data('sirtrevor-defaults'),
-            conf = $el.data('sirtrevor-conf'),
-            st,
-            options;
 
-        SirTrevor.setDefaults(defaults);
-        options = _.extend({}, conf, {el: $el});
-        st = new SirTrevor.Editor(options);
-    });
-}(jQuery));
+var bowtieContentController = {
+
+    initialize: function() {
+        $('.js-st-instance').each(function(i, el) {
+            var $el = $(el);
+            var defaults = $el.data('sirtrevor-defaults');
+            var conf = $el.data('sirtrevor-conf');
+            var options;
+
+            SirTrevor.setDefaults(defaults);
+            options = _.extend({}, conf, {el: $el});
+
+            new SirTrevor.Editor(options);
+        });
+    }
+
+};
+
+$(document).ready(function () {
+    bowtieContentController.initialize();
+});
